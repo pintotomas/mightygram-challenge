fe4f84b71df5ec531106659a0220afce18599a04
pintotomas <tomasp834@gmail.com>
2021-10-30 02:10:31 -0300

    fix schema (user is reserved in postgres) add data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,283 @@
+ INSERT INTO users
+            (id,
+             created,
+             updated,
+             username)
+VALUES      (1,
+             '1/19/2021',
+             '12/22/2020',
+             'gfraczkiewicz0');
+
+INSERT INTO users
+            (id,
+             created,
+             updated,
+             username)
+VALUES      (2,
+             '12/4/2020',
+             '7/31/2021',
+             'mjonin1');
+
+INSERT INTO users
+            (id,
+             created,
+             updated,
+             username)
+VALUES      (3,
+             '5/21/2021',
+             '5/31/2021',
+             'jchippin2');
+
+INSERT INTO users
+            (id,
+             created,
+             updated,
+             username)
+VALUES      (4,
+             '10/10/2021',
+             '8/28/2021',
+             'nmullard3');
+
+INSERT INTO users
+            (id,
+             created,
+             updated,
+             username)
+VALUES      (5,
+             '2/25/2021',
+             '10/15/2021',
+             'rguerreru4');
+
+INSERT INTO post
+            (id,
+             created,
+             updated,
+             description,
+             photo_url)
+VALUES      (1,
+             '5/5/2021',
+             '12/16/2020',
+             'Ágios Týchon',
+             'http://dummyimage.com/189x244.png/cc0000/ffffff');
+
+INSERT INTO post
+            (id,
+             created,
+             updated,
+             description,
+             photo_url)
+VALUES      (2,
+             '2/5/2021',
+             '11/18/2020',
+             'Syntul',
+             'http://dummyimage.com/163x128.png/dddddd/000000');
+
+INSERT INTO post
+            (id,
+             created,
+             updated,
+             description,
+             photo_url)
+VALUES      (3,
+             '11/21/2020',
+             '12/11/2020',
+             'Sidowayah Lor',
+             'http://dummyimage.com/152x139.png/dddddd/000000');
+
+INSERT INTO post
+            (id,
+             created,
+             updated,
+             description,
+             photo_url)
+VALUES      (4,
+             '4/16/2021',
+             '2/12/2021',
+             'Ludza',
+             'http://dummyimage.com/121x170.png/ff4444/ffffff');
+
+INSERT INTO post
+            (id,
+             created,
+             updated,
+             description,
+             photo_url)
+VALUES      (5,
+             '5/19/2021',
+             '9/5/2021',
+             'Zhihe',
+             'http://dummyimage.com/209x240.png/cc0000/ffffff');
+
+INSERT INTO post
+            (id,
+             created,
+             updated,
+             description,
+             photo_url)
+VALUES      (6,
+             '7/11/2021',
+             '11/11/2020',
+             'Bang Len',
+             'http://dummyimage.com/204x165.png/dddddd/000000');
+
+INSERT INTO post
+            (id,
+             created,
+             updated,
+             description,
+             photo_url)
+VALUES      (7,
+             '7/8/2021',
+             '7/29/2021',
+             'Furudate',
+             'http://dummyimage.com/230x214.png/dddddd/000000');
+
+INSERT INTO post
+            (id,
+             created,
+             updated,
+             description,
+             photo_url)
+VALUES      (8,
+             '5/27/2021',
+             '4/22/2021',
+             'Donetsk',
+             'http://dummyimage.com/239x104.png/ff4444/ffffff');
+
+INSERT INTO post
+            (id,
+             created,
+             updated,
+             description,
+             photo_url)
+VALUES      (9,
+             '5/14/2021',
+             '4/17/2021',
+             'Nandayure',
+             'http://dummyimage.com/235x229.png/ff4444/ffffff');
+
+INSERT INTO post
+            (id,
+             created,
+             updated,
+             description,
+             photo_url)
+VALUES      (10,
+             '6/24/2021',
+             '8/6/2021',
+             'Manūjān',
+             'http://dummyimage.com/127x162.png/dddddd/000000');
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (1,
+             1);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (1,
+             3);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (1,
+             4);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (1,
+             6);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (2,
+             1);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (2,
+             2);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (2,
+             8);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (3,
+             8);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (3,
+             4);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (3,
+             5);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (4,
+             7);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (4,
+             9);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (5,
+             1);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (5,
+             2);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (5,
+             3);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (5,
+             4);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (5,
+             6);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (5,
+             7);
+
+INSERT INTO user_post
+            (id_user,
+             id_post)
+VALUES      (5,
+             8);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,20 @@
-CREATE TABLE "user" (
+DROP TABLE IF EXISTS "user_post";
+DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "post";
+
+CREATE TABLE "users" (
   "id" bigint PRIMARY KEY,
   "created" timestamp,
   "updated" timestamp,
-  "username" varchar UNIQUE NOT NULL
+  "username" varchar(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE "post" (
   "id" bigint PRIMARY KEY,
   "created" timestamp,
   "updated" timestamp,
-  "description" varchar(1500),
-  "photo_url" varchar(2048)
+  "description" varchar(1500) NOT NULL,
+  "photo_url" varchar(2048) NOT NULL
 );
 
 CREATE TABLE "user_post" (
@@ -18,6 +22,6 @@ CREATE TABLE "user_post" (
   "id_post" bigint
 );
 
-ALTER TABLE "user_post" ADD FOREIGN KEY ("id_user") REFERENCES "user" ("id");
+ALTER TABLE "user_post" ADD FOREIGN KEY ("id_user") REFERENCES "users" ("id");
 
 ALTER TABLE "user_post" ADD FOREIGN KEY ("id_post") REFERENCES "post" ("id");
